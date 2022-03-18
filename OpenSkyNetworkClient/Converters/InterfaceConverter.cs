5eec66691c8e63a8e6ddf8c30f3f8d7a38cbac00
Emil Rasmussen <emil@erasmussen.dk>
2022-03-18 13:31:01 +0100

    Created InterfaceConverter Class

---
+++ OpenSkyNetworkClient/Converters/InterfaceConverter.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSkyNetworkClient.Converters
+{
+    class InterfaceConverter<TInterface, TClass> : CustomCreationConverter<TInterface>
+        where TClass : TInterface, new()
+    {
+        public override TInterface Create(Type objectType) => new TClass();
+    }
+}
