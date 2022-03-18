e5a0154c266a0a3fa047852fc56175393af01bd9
Emil Rasmussen <emil@erasmussen.dk>
2022-03-18 13:29:12 +0100

    added IFlightStates Interface

---
+++ OpenSkyNetworkClient/Interfaces/IFlightStates.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSkyNetworkClient.Interfaces
+{
+    [JsonConverter(typeof(InterfaceConverter<IFlightStates, FlightStates>))]
+    public interface IFlightStates
+    {
+        [JsonConverter(typeof(UnixDateTimeConverter))]
+        [JsonProperty(PropertyName = "time")]
+        DateTime Time { get;}
+
+        [JsonConverter(typeof(FlightStateArrayConverter))]
+        IFlightState[] States { get; }
+    }
+}
