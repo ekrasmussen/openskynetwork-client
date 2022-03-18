b0fd5ff9d095d6202d6a34c944b80feade5f0c8b
Emil Rasmussen <emil@erasmussen.dk>
2022-03-18 13:58:29 +0100

    Added Connection Abstract

---
+++ OpenSkyNetworkClient/APIAccess/Connection.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSkyNetworkClient.APIAccess
+{
+    public abstract class Connection : IDisposable
+    {
+        const string baseurl = "opensky-network.org/api/";
+
+        readonly HttpClient client;
+        readonly JsonSerializer serializer;
+
+        protected bool HasCredentials => client.DefaultRequestHeaders.Authorization != null;
+
+
+        protected Connection()
+        {
+            client = new HttpClient(new HttpClientHandler());
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.BaseAddress = new Uri("https://" + baseurl);
+
+            serializer = new JsonSerializer();
+        }
+
+        protected Connection(string username, string password)
+            : this()
+        {
+            if (string.IsNullOrWhiteSpace(username))
+                throw new ArgumentNullException(nameof(username));
+
+            if (string.IsNullOrWhiteSpace(password))
+                throw new ArgumentNullException(nameof(password));
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes($"{username}:{password}")));
+        }
+
+
+        protected async Task<TResult> GetAsync<TResult>(string uriPath, CancellationToken token = default)
+        {
+            var response = await client.GetAsync(uriPath, HttpCompletionOption.ResponseHeadersRead, token).ConfigureAwait(false);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using (var stream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false))
+                using (var sr = new StreamReader(stream))
+                using (var jtr = new JsonTextReader(sr))
+                {
+                    return serializer.Deserialize<TResult>(jtr);
+                }
+            }
+
+            throw new OpenSkyNetException($"{ response.StatusCode } - {response.ReasonPhrase}");
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        protected virtual void OnDispose()
+        {
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                OnDispose();
+
+                client?.Dispose();
+            }
+        }
+    }
+}
---
+++ OpenSkyNetworkClient/OpenSkyNetException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSkyNetworkClient
+{
+    public class OpenSkyNetException : Exception
+    {
+
+        public OpenSkyNetException()
+        {
+        }
+
+
+        public OpenSkyNetException(string message)
+            : base(message)
+        {
+        }
+
+
+        public OpenSkyNetException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+#if !COREFX
+
+        protected OpenSkyNetException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+#endif
+    }
+}
+
