ba63c9cf8059157ee17d9e6a1c01780850bc8a44
Emil Rasmussen <emil@erasmussen.dk>
2022-03-21 17:04:26 +0100

    commented out test code

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -4,10 +4,10 @@ using OpenSkyNetworkClient;
 OpenSkyNetClient client = new OpenSkyNetClient();
 
 
-var flights = await client.GetAllStatesAsync();
+//var flights = await client.GetAllStatesAsync();
 
-string[] icaos = { "aae327", "ab5ce4" };
+//string[] icaos = { "aae327", "ab5ce4" };
 
-var customFlights = await client.GetCustomStatesAsync(icaos);
+//var customFlights = await client.GetCustomStatesAsync(icaos);
 
-Console.WriteLine(flights.States[0].CallSign);
+//Console.WriteLine(flights.States[0].CallSign);
